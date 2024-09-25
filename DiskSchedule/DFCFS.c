4a75167f7b3863749b2bf843b87c4728b2bb7a8b
SoulSourav <130975907+SouravH5409@users.noreply.github.com>
2024-09-25 15:25:23 +0530

    Create DFCFS.c

---
+++ DiskSchedule/DFCFS.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_REQUESTS 100
+
+int calculateTotalSeekTime(int *requests, int numRequests) {
+    int totalSeekTime = 0;
+    int currentTrack = 0;
+    for (int i = 0; i < numRequests; i++) {
+                int SeekTime = abs(currentTrack - requests[i]); 
+                currentTrack =requests[i]; 
+            totalSeekTime+=SeekTime;
+        }
+    return totalSeekTime;
+}
+
+int main() {
+    int requests[MAX_REQUESTS];
+    int numRequests;
+    printf("Enter the number of disk requests: ");
+    scanf("%d", &numRequests);
+
+    if (numRequests <= 0 || numRequests > MAX_REQUESTS) {
+        printf("Invalid number of requests.\n");
+        return 1;
+    }
+    printf("Enter the disk requests (track numbers):\n");
+    for (int i = 0; i < numRequests; i++) {
+        scanf("%d", &requests[i]);
+    }
+
+  int totalSeekTime = calculateTotalSeekTime(requests, numRequests);
+    printf("Total seek time using SCAN: %d\n", totalSeekTime);
+
+    return 0;
+}
